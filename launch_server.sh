5a59a2716f1026afa1410dfa538ebb6b551e7936
grew <grew@quarm>
2026-01-17 18:11:25 +0000

    luclin zones

--- launch_server.sh
+++ launch_server.sh
@@ -6,16 +6,18 @@
 ./build/bin/shared_memory &
 sleep 6
 
-#./build/bin/loginserver &
-#sleep 3
+./build/bin/loginserver &
+sleep 3
 
 ./build/bin/world &
 sleep 3
 
+./luclin_zones.sh &
+sleep 60
 ./starting_zones.sh &
-sleep 1
+sleep 120
 ./classic_zones.sh &
-sleep 1
+sleep 120
 ./velious_zones.sh &
-sleep 1
+sleep 60
 ./build/bin/eqlaunch dynzone1 &
---
+++ luclin_zones.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+# Script: luclin_zones
+# Purpose: Launch multiple background processes for Luclin zone operations.
+
+./build/bin/zone shadowhaven &
+echo "Launching zone: shadowhaven"
+sleep 3
+
+./build/bin/zone nexus &
+echo "Launching zone: nexus"
+sleep 3
+
+./build/bin/zone echo &
+echo "Launching zone: echo"
+sleep 3
+
+./build/bin/zone acrylia &
+echo "Launching zone: acrylia"
+sleep 3
+
+./build/bin/zone sharvahl &
+echo "Launching zone: sharvahl"
+sleep 3
+
+./build/bin/zone paludal &
+echo "Launching zone: paludal"
+sleep 3
+
+./build/bin/zone fungusgrove &
+echo "Launching zone: fungusgrove"
+sleep 3
+
+./build/bin/zone vexthal &
+echo "Launching zone: vexthal"
+sleep 3
+
+./build/bin/zone sseru &
+echo "Launching zone: sseru"
+sleep 3
+
+./build/bin/zone katta &
+echo "Launching zone: katta"
+sleep 3
+
+./build/bin/zone netherbian &
+echo "Launching zone: netherbian"
+sleep 3
+
+./build/bin/zone ssratemple &
+echo "Launching zone: ssratemple"
+sleep 3
+
+./build/bin/zone griegsend &
+echo "Launching zone: griegsend"
+sleep 3
+
+./build/bin/zone thedeep &
+echo "Launching zone: thedeep"
+sleep 3
+
+./build/bin/zone shadeweaver &
+echo "Launching zone: shadeweaver"
+sleep 3
+
+./build/bin/zone hollowshade &
+echo "Launching zone: hollowshade"
+sleep 3
+
+./build/bin/zone grimling &
+echo "Launching zone: grimling"
+sleep 3
+
+./build/bin/zone mseru &
+echo "Launching zone: mseru"
+sleep 3
+
+./build/bin/zone letalis &
+echo "Launching zone: letalis"
+sleep 3
+
+./build/bin/zone twilight &
+echo "Launching zone: twilight"
+sleep 3
+
+./build/bin/zone thegrey &
+echo "Launching zone: thegrey"
+sleep 3
+
+./build/bin/zone tenebrous &
+echo "Launching zone: tenebrous"
+sleep 3
+
+./build/bin/zone maiden &
+echo "Launching zone: maiden"
+sleep 3
+
+./build/bin/zone dawnshroud &
+echo "Launching zone: dawnshroud"
+sleep 3
+
+./build/bin/zone scarlet &
+echo "Launching zone: scarlet"
+sleep 3
+
+./build/bin/zone umbral &
+echo "Launching zone: umbral"
+sleep 3
+
+./build/bin/zone akheva &
+echo "Launching zone: akheva"
+sleep 3
